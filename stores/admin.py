784e2ff620f8c1a54af0f3abfba5829f169614db
daryhaя <daryha56@gmail.com>
2023-12-29 09:21:25 +0600

    Initial commit

--- stores/admin.py
+++ stores/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Store, Restaurant, Category, Product
+from .models import Store, Restaurant, Category, Product, Order,  OrderItem
 
 @admin.register(Store)
 class StoreAdmin(admin.ModelAdmin):
@@ -22,3 +22,16 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ['name', 'price', 'category', 'store', 'restaurant']
     search_fields = ['name']
     list_filter = ['category', 'store', 'restaurant']
+
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['user', 'first_name', 'last_name', 'address', 'city', 'phone_num', 'postal_code', 'created_at', 'updated_at', 'total_price']
+    search_fields = ['user__username', 'first_name', 'last_name', 'address', 'city', 'phone_num']
+    list_filter = ['created_at', 'updated_at']
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ['order', 'product', 'price', 'quantity']
+    search_fields = ['order__user__username', 'product__name']
